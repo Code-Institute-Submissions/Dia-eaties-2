290e479ef196cfdb20c5e9ad001360a28aafe651
Diabekki <bekkitubridy@gmail.com>
2022-01-30 22:38:47 +0000

    Added comments and appoval of comments to admin

--- diaeaties2app/admin.py
+++ diaeaties2app/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Recipe, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
+
+    list_display = ('recipe_name', 'slug', 'status', 'created')
+    prepopulated_fields = {'slug': ('recipe_name',)}
+    search_fields = ['recipe_name', 'content']
+    list_filter = ('status', 'created')
     summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('display_name', 'comment_text', 'recipe', 'created', 'approved')
+    search_fields = ('display_name', 'email', 'comment_text')
+    list_filter = ('approved', 'created')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
