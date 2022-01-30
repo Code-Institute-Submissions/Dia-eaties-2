9cccce3a8d850744af4639bd2794822b9f5762be
Diabekki <bekkitubridy@gmail.com>
2022-01-30 23:32:22 +0000

    Added static, templates and modified urls

--- Dia_eaties_2/urls.py
+++ Dia_eaties_2/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('', include('diaeaties2app.urls'), name='diaeaties2app_urls'),
 ]
