84ea46d1999af08baa9336aee0fb30c9e42486d2
Diabekki <bekkitubridy@gmail.com>
2022-01-31 22:01:47 +0000

    Finshed making the recipe_post detail template

--- diaeaties2app/views.py
+++ diaeaties2app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Recipe
 
 class RecipeList(generic.ListView):
@@ -7,3 +7,23 @@ class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by('-created')
     template_name = 'index.html'
     paginate_by = 2
+
+
+class RecipeDetail(View):
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.filter(approved=True).order_by('created')
+        loved = False
+        if recipe.loves.filter(id=self.request.user.id).exists():
+            loved = True
+        
+        return render (
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "loved": loved
+            },
+        )
