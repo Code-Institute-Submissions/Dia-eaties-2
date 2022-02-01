85e2950afdf0737c9d3f03791b3ad278dc3e917e
Diabekki <bekkitubridy@gmail.com>
2022-02-01 19:10:30 +0000

    Added love functionality to recipe posts

--- diaeaties2app/views.py
+++ diaeaties2app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Recipe
 from .forms import CommentForm
 
@@ -25,7 +26,50 @@ class RecipeDetail(View):
             {
                 "recipe": recipe,
                 "comments": comments,
+                "commented": False,
                 "loved": loved,
                 "comment_form": CommentForm()
             },
         )
+
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.filter(approved=True).order_by('created')
+        loved = False
+        if recipe.loves.filter(id=self.request.user.id).exists():
+            loved = True
+
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.display_name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.recipe = recipe
+            comment.save()
+        else:
+            comment_form = CommentForm()
+        
+        return render (
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "commented": True,
+                "loved": loved,
+                "comment_form": CommentForm()
+            },
+        )
+
+class LovedRecipe(View):
+    
+    def post(self, request, slug, *args, **kwargs):
+        recipe = get_object_or_404(Recipe, slug=slug)
+        if recipe.loves.filter(id=request.user.id).exists():
+            recipe.loves.remove(request.user)
+        else:
+            recipe.loves.add(request.user)
+
+        return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
